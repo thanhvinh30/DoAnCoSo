4b6afb58d2d8a28cc7c1865128fba03c1ddca0f9
thanhvinh30 <thanhvinh69215@gmail.com>
2024-09-15 17:28:01 +0700

    Fix link products => (shop.html)

--- DoAnCoSo/DoAnCoSo/Controllers/ProductsController.cs
+++ DoAnCoSo/DoAnCoSo/Controllers/ProductsController.cs
@@ -17,34 +17,99 @@ namespace DoAnCoSo.Controllers
         {
             _context = context;
         }
+        [Route("/shop.html", Name = "Shop-Products")]
         public IActionResult Products(int? page)
         {
+
+
+            //int pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            //int pageSize = 6;
+            //var lsproducts = db.Products
+            //    .AsNoTracking()
+            //    .OrderBy(x => x.ProId)
+            //    .ToList();
+            //// .ToPagedList(pageNumber, pageSize);
+
+            //PagedList<Product> lstpro = new PagedList<Product>(lsproducts.AsQueryable(), pageSize, pageNumber);
+            //ViewBag.CurrentPage = pageNumber; // Để giữ giá trị phân loại trong view
+            //return View(lsproducts);
+            // Dùng try catch, nếu có lỗi thì dễ xử lí hơn
+            try
+            {
             int pageNumber = page  == null || page <= 0 ? 1 : page.Value;
             int pageSize = 6;
             var lsproducts = db.Products
                 .AsNoTracking()
-                .OrderBy(x => x.ProId)
-                .ToList();
+                .OrderByDescending(x => x.DateCreated);
+                //.ToList();
                // .ToPagedList(pageNumber, pageSize);
 
-            PagedList<Product> lstpro = new PagedList<Product>(lsproducts.AsQueryable(), pageSize, pageNumber);
+            PagedList<Product> models = new PagedList<Product>(lsproducts, pageSize, pageNumber);
             ViewBag.CurrentPage = pageNumber; // Để giữ giá trị phân loại trong view
-            return View(lsproducts);
+            return View(models);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+          
         }
         public IActionResult Cart()
         {
             return View();
         }
+        [Route("/{Alias}-{id}", Name = "ListProducts")]
+        public IActionResult List(int id, int page = 1)
+        {
+                try
+                {
+                    //var pageNumber = page == null page <= 0 ? 1 : page.Value;
+                    var pageSize = 6;
+                    var Category = _context.Categories.Find(id);
+                    var lsCate = _context.Products  .AsNoTracking()
+                                                    .Where( x => x.CatId == id)
+                                                    .OrderByDescending(x => x.DateCreated);
+
+
+                    PagedList<Product> models = new PagedList<Product>(lsCate, pageSize, page);
+                    ViewBag.CurrentPage = page; // Để giữ giá trị phân loại trong view
+                    ViewBag.CurrentCate = Category;
+                    return View(models);
+                }
+                catch
+                {
+                     return RedirectToAction("Index", "Home");
+                }
+           
+
+        }
+        [Route("/{Alias}-{id}.html", Name ="ProductsDetail")]
         public IActionResult Details(int id)
         {
-            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProId == id);
-
-            if (product == null)
+            try
             {
-                return RedirectToAction("Index");
-            }
+                var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProId == id);
 
-            return View();
+                if (product == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProId == id);
+
+            //if (product == null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+
+            //return View();
         }
         public IActionResult Checkout()
         {
--- DoAnCoSo/DoAnCoSo/obj/Debug/net8.0/DoAnCoSo.AssemblyInfo.cs
+++ DoAnCoSo/DoAnCoSo/obj/Debug/net8.0/DoAnCoSo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnCoSo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f998dd9e61e1881adfb83086af0e90312b2e556a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88877427c59a229f39b39c94126bcb20088762f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnCoSo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnCoSo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
